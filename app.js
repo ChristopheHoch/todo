710d96be12410224deec8eff8ca048ec990eeda6
Christophe Hoch <christophe.hoch@gmail.com>
2013-08-25 09:40:54 +0200

    Rest API fully working with ember

--- app.js
+++ app.js
@@ -26,44 +26,76 @@ mongoose.connect('mongodb://test:test@paulo.mongohq.com:10019/todo');
 
 db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', function callback () {
-  console.log("Connection openned...");
+db.once('open', function callback() {
+	console.log("Connection openned...");
+
+	var todoSchema = mongoose.Schema({
+		title: String,
+		is_completed: Boolean
+	});
+	var Todo = mongoose.model('Todo', todoSchema);
+	
+	app.get('/todos', function (req, res) {
+		Todo.find(function (err, todos) {
+			if (!err) {
+				console.log('Found todos: ' + JSON.stringify(todos));
+				return res.send({todos: todos});
+			} else {
+				return console.log('Error: Todos not found: ' + err);
+			}
+		});
+	});
+	
+	app.post('/todos', function (req, res) {
+		var todo = new Todo(req.body.todo);
+		todo.save(function (err) {
+			if (!err) {
+				return console.log("Todo created!");
+			} else {
+				console.log('Error: Todo not created: ' + err);
+				return console.log(err);
+			}
+		});
+		return res.send({todo: todo});
+	});
+	
+	app.put('/todos/:id', function (req, res) {
+		return Todo.findById(req.params.id, function (err, todo) {
+			todo.title = req.body.todo.title;
+			todo.is_completed = req.body.todo.is_completed;
+			return todo.save(function (err) {
+				if (!err) {
+					console.log("Todo updated!");
+				} else {
+					console.log('Error: Todo not updated: ' + err);
+					console.log(err);
+				}
+				return res.send({todo: todo});
+			});
+		});
+	});
+	
+	app.delete('/todos/:id', function (req, res) {
+		return Todo.findById(req.params.id, function (err, todo) {
+			return todo.remove(function (err) {
+				if (!err) {
+					console.log("Todo deleted!");
+					return res.send('');
+				} else {
+					console.log('Error: Todo not deleted: ' + err);
+					console.log(err);
+				}
+			});
+		});
+	});
+	
 });
-
-var todoSchema = mongoose.Schema({
-	title: String,
-	is_completed: Boolean
-});
-var Todo = mongoose.model('Todo', todoSchema);
-
-app.get('/todos', function (req, res) {
-   return Todo.find(function (err, todo) {
-	 if (!err) {
-	   console.log('Found todo: ' + JSON.stringify(todo));
-	   return res.send({todos: todo});
-	 } else {
-	   return console.log('Error: Todo not found: ' + err);
-	 }
-   });
- });
-
-app.post('/todos', function (req, res) {
-	var todo = new Todo(req.body.todo);
-	todo.save(function (err) {
-		if (!err) {
-		  return console.log("Todo created!");
-		} else {
-		  return console.log(err);
-		}
-	  });
-	  return res.send({todo: todo});
- });
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });
--- public/js/models/store.js
+++ public/js/models/store.js
@@ -1,3 +1,11 @@
+Todos.Adapter = DS.RESTAdapter.extend({
+	serializer: DS.RESTSerializer.extend({
+		primaryKey: function (type) {
+			return '_id';
+		}
+	})
+});
+
 Todos.Store = DS.Store.extend({
-//  adapter: 'DS.FixtureAdapter'
+  adapter: 'Todos.Adapter'
 });
